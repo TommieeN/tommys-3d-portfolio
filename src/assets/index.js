11f93952f6da12dfb5cd1ca02d4300a00ec60c38
TommyNguyen <timsnguyen@hotmail.com>
2023-04-20 21:00:29 -0700

    finish rotating balls and info

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,23 +8,22 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
+import express from "./tech/express.svg";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
+import sass from "./tech/sass.svg";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
+import mysql from "./tech/mysql.svg";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import fortuneCooking from "./company/fortuneCooking.png";
+import nofrills from "./company/noFrills.png";
+import costco from "./company/costco.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -40,22 +39,21 @@ export {
   menu,
   close,
   css,
-  docker,
+  express,
   figma,
   git,
   html,
   javascript,
-  mongodb,
+  sass,
   nodejs,
   reactjs,
-  redux,
+  mysql,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  fortuneCooking,
+  nofrills,
+  costco,
   carrent,
   jobit,
   tripguide,
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+      >
+        Hi there! I'm a recent graduate of a full stack web development bootcamp
+        at BrainStation, with experience in Javascript, Node, React, and more.
+        I'm currently learning how to use Three.js to take my skills to the next
+        level as you can see. I'm passionate about web development and always looking for ways
+        to learn and improve. I'm eager to bring my skills and enthusiasm to any
+        team or project, and create user-friendly and visually appealing
+        websites.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
